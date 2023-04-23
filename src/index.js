859684883bc78ff7a7494cb2b94d1a38fe1027d4
dombott <73060470+dombott@users.noreply.github.com>
2023-04-23 18:47:07 +0200

    change import of css files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import './css/structure.css';
-import './css/typography.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
